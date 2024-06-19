62d52d780d829368b3113133aabd23d8df6751a0
Muhammad Rizal Nur Wakhid <82570585+rizalwahid68@users.noreply.github.com>
2024-06-19 23:06:35 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // src/app/page.tsx
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -7,15 +7,10 @@ export default function Home() {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://my-nextjs-frame.vercel.app/park-2.png" />
         <meta property="og:image" content="https://my-nextjs-frame.vercel.app/park-2.png" />
-        <meta property="fc:frame:post_url" content="https://my-nextjs-frame.vercel.app/api/basic?id=1" />
-        <meta property="fc:frame:button:1" content="Start" />
-        <meta property="fc:frame:button:1:url" content="https://my-nextjs-frame.vercel.app/park-3.png" />
         <title>Simple Frame</title>
       </Head>
       <h1>Simple Warpcast Frame</h1>
     </>
-  )
+  );
 }
