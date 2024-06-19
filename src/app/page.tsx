ccc639950f07785a509b0ad9fe3bb95a4099e2e0
Muhammad Rizal Nur Wakhid <82570585+rizalwahid68@users.noreply.github.com>
2024-06-19 22:54:03 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="/park-2.png" />
-        <meta property="og:image" content="/park-2.png" />
-        <meta property="fc:frame:post_url" content="/api/basic?id=1" />
+        <meta property="fc:frame:image" content="https://my-nextjs-frame.vercel.app/park-2.png" />
+        <meta property="og:image" content="https://my-nextjs-frame.vercel.app/park-2.png" />
+        <meta property="fc:frame:post_url" content="https://my-nextjs-frame.vercel.app/api/basic?id=1" />
         <meta property="fc:frame:button:1" content="Start" />
-        <meta property="fc:frame:button:1:url" content="/park-3.png" />
+        <meta property="fc:frame:button:1:url" content="https://my-nextjs-frame.vercel.app/park-3.png" />
         <title>Simple Frame</title>
       </Head>
       <h1>Simple Warpcast Frame</h1>
